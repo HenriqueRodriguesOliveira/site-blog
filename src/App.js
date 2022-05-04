a564e0c2b5af79a50775caf175a7aac577e6e441
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2022-05-04 13:25:41 -0300

    blog

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 
-import Paths from './paths';
+import './App.css';
 
-import { useEffect } from 'react';
-
-import Provider from 'pages/Provider';
-
-import ScriptTag from 'react-script-tag';
-
-function App() {
-/*
-    // useEffect para carregar o script do menu responsivo
-    useEffect(() => {
-      const script = document.createElement('script');
-  
-      script.src = "./js/script.js";
-      script.async = true;
-  
-      document.body.appendChild(script);
-      
-      return () => {
-        document.body.removeChild(script);
-      }
-  
-    }, []);
-
-*/
+function App(){
   return (
     <>
-    <ScriptTag type="text/javascript" src="./js/script.js" />
-    <Provider>
-      <Paths />
-    </Provider>
+    
     </>
-  );
+  )
 }
 
 export default App;
